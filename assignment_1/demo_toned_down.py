59ce44495b122a572c8e279f86d5dec52c4a987c
Kikis <kikispcmr@gmail.com>
2024-05-05 16:29:08 +0200

    Final formatting.

--- assignment_1/demo_toned_down.py
+++ assignment_1/demo_toned_down.py
@@ -1,14 +1,10 @@
-import re
-
 from autobahn.twisted.component import Component, run
 from autobahn.twisted.util import sleep
-from autobahn.wamp.request import Subscription
 from twisted.internet.defer import inlineCallbacks
 
-TIMEOUT_TIME = 1
 wamp = Component(
     transports=[{"url": "ws://wamp.robotsindeklas.nl", "serializers": ["msgpack"]}],
-    realm="rie.6636524dc887f6d074f03f25",
+    realm="rie.6633488cc887f6d074f02eeb",
 )
 
 # Questions: (Question asked: string, Question truth: boolean, Trivia Reply: string)
@@ -53,27 +49,17 @@ change_flow = [("Do you want to continue or change the game up?", True)]
 
 
 @inlineCallbacks
-def smart_question_flow(session, question):
-    yield sleep(1)
-
-    print("WE ARE HERE")
-    answer = yield session.call(
-        "rie.dialogue.ask",
-        question=question[0],
-        answers={
-            "true": ["true", "yes", "ja", "tru"],
-            "false": ["false", "no", "nej", "fls"],
-        },
-    )
-    print("WE ARE HERE2", answer)
-    yield sleep(1)
-    data = yield session.call("rie.dialogue.stt.read", time=TIMEOUT_TIME)
-    print(data, answer)
-    return data, answer
-
-
-@inlineCallbacks
 def smart_question_binary(session, question):
+    """
+    Asks a binary (True/False) question to the user and provides feedback based on the user's answer.
+
+    Args:
+        session (autobahn.twisted.component.Component): The WAMP session object.
+        question (tuple): A tuple containing the question text, the correct answer (True or False), and a trivia reply.
+
+    Returns:
+        None
+    """
     yield sleep(1)
     answer = yield session.call(
         "rie.dialogue.ask",
@@ -84,10 +70,10 @@ def smart_question_binary(session, question):
         },
     )
     yield sleep(1)
-    data = yield session.call("rie.dialogue.stt.read", time=TIMEOUT_TIME)
+    _ = yield session.call("rie.dialogue.stt.read", time=6000)
 
     if (answer == "true" and question[1]) or (answer == "false" and not question[1]):
-        # nod yes
+        # Nod yes
         session.call(
             "rom.actuator.motor.write",
             frames=[
@@ -104,9 +90,17 @@ def smart_question_binary(session, question):
         yield session.call("rie.dialogue.say", text="That is incorrect.")
 
 
-@inlineCallbacks
 def smart_question_multiple(session, question):
-    _, answer = smart_question_flow(session, question)
+    """
+    Asks a multiple-choice question to the user and provides feedback based on the user's answer.
+
+    Args:
+        session (autobahn.twisted.component.Component): The WAMP session object.
+        question (tuple): A tuple containing the question text, a list of correct answers, and a trivia reply.
+
+    Returns:
+        None
+    """
     yield sleep(1)
     answer = yield session.call(
         "rie.dialogue.ask",
@@ -114,9 +108,10 @@ def smart_question_multiple(session, question):
         answers={"true": ["true", "yes", "ja"], "false": ["false", "no", "nej"]},
     )
     yield sleep(1)
-    yield session.call("rie.dialogue.stt.read", time=TIMEOUT_TIME)
+    _ = yield session.call("rie.dialogue.stt.read", time=6000)
 
     if answer in question[1]:
+        # nod yes
         session.call(
             "rom.actuator.motor.write",
             frames=[
@@ -127,47 +122,65 @@ def smart_question_multiple(session, question):
             ],
             force=True,
         )
+
         text = "That is correct." + question[2]
+
         yield session.call("rie.dialogue.say", text=text)
     elif answer not in question[1]:
         yield session.call("rie.dialogue.say", text="That is incorrect.")
 
 
 @inlineCallbacks
-def smart_question_branching(session, question):
+def smart_question_branching(session):
+    """
+    Asks the user if they want to try something harder and branches the flow based on the user's answer.
+
+    Args:
+        session (autobahn.twisted.component.Component): The WAMP session object.
+
+    Returns:
+        bool: True if the user wants to try something harder, False otherwise.
+    """
     yield sleep(1)
     answer = yield session.call(
         "rie.dialogue.ask",
-        question=question[0],
+        question="Do you want to try something harder?",
         answers={
             "true": ["true", "tru", "yes", "ye"],
             "false": ["false", "no", "na", "nej"],
         },
     )
     yield sleep(1)
-    data = yield session.call("rie.dialogue.stt.read", time=TIMEOUT_TIME)
-
+    _ = yield session.call("rie.dialogue.stt.read", time=6000)
     answ = None
 
-    for frame in data:
-        if frame["data"]["body"]["final"]:
-            print(frame)
-
+    # If true we do a different set of questions, if false continue, otherwise ask again
     if answer == "true":
         text = "That's great! Let's go with some trivia."
         yield session.call("rie.dialogue.say", text=text)
         answ = True
     elif answer == "false":
         yield session.call("rie.dialogue.say", text="No problem, let's continue!")
-        anws = False
+        answ = False
     else:
-        smart_question_branching(session, question)
+        smart_question_branching(session)
 
     return answ
 
 
+# Handles keyword input
 @inlineCallbacks
 def on_keyword(frame, session):
+    """
+    Callback function for the keyword event.
+
+    Args:
+        frame (dict): The frame containing the detected keyword and its certainty.
+        session (autobahn.twisted.component.Component): The WAMP session object.
+
+    Returns:
+        None
+    """
     if (
         "certainty" in frame["data"]["body"]
         and frame["data"]["body"]["certainty"] > 0.45
@@ -178,14 +191,26 @@ def on_keyword(frame, session):
         )
 
 
+# Run keyword question by the robot
 @inlineCallbacks
 def keyword(session, statement, keywords):
+    """
+    Asks the user a statement and waits for a keyword response.
+
+    Args:
+        session (autobahn.twisted.component.Component): The WAMP session object.
+        statement (str): The statement to present to the user.
+        keywords (list): A list of keywords to listen for.
+
+    Returns:
+        tuple: A tuple containing the user's response, the certainty of the response, and a boolean indicating if the response is final.
+    """
     ans = None
     cert = None
     final = None
     yield session.call("rie.dialogue.say", text=statement)
     yield sleep(0.5)
-    data = yield session.call("rie.dialogue.stt.read", time=TIMEOUT_TIME)
+    data = yield session.call("rie.dialogue.stt.read", time=6000)
     for frame in data:
         if frame["data"]["body"]["final"]:
             print(frame)
@@ -201,64 +226,61 @@ def keyword(session, statement, keywords):
     return ans, cert, final
 
 
-def regex(session, yes_pattern, no_pattern, reply):
-    if re.search(yes_pattern, reply):
-        # Trivia questions
-        yield session.call("rie.dialogue.say", text="Let's start with some trivia!")
-        print("START TRIVIA QUESTIONS")
-    elif re.search(no_pattern, reply):
-        # Continue with normal questions
-        yield session.call("rie.dialogue.say", text="Alright, let's continue!")
-        print("CONTINUE")
-    else:
-        # For times it throws a random event from the api
-        yield sleep(1)
-        yield session.call(
-            "rie.dialogue.say",
-            text="I'm going to assume you want to keep on playing! We are having so much fun!",
-        )
-
-
 @inlineCallbacks
-def main(session, details):
+def main(session, _):
+    """
+    The main function that runs the dialogue flow.
+
+    Args:
+        session (autobahn.twisted.component.Component): The WAMP session object.
+        _ (Any): An unused argument required by the autobahn library.
+
+    Returns:
+        None
+    """
     session.call("rom.optional.behavior.play", name="BlocklyStand")
     session.call("rie.vision.face.find")
     session.call("rie.vision.face.track")
 
+    # Start of Dialogue flow
     first_key = ["start", "yes", "ja", "go", "forward", "play"]
-    reply, cert, final = yield keyword(
+    _, _, final = yield keyword(
         session, "Say start when you're ready to play!", first_key
     )
 
     if final:
-        yield session.call("rie.dialogue.say", text="Let's gooo")
-        print("We starts")
+        yield session.call("rie.dialogue.say", text="Let's go!")
 
+    # Basic question track with additional trivia replies
     yield smart_question_binary(session, questions[0])
     yield smart_question_binary(session, questions[1])
     yield smart_question_binary(session, questions[2])
 
-    answ = yield smart_question_branching(
-        session, ["Do you want to try something harder?"]
-    )
+    # Branching question: If true then do trivia questions otherwise continue regular questions
+    answ = yield smart_question_branching(session)
 
     if answ:
         yield smart_question_binary(session, trivia[0])
         yield smart_question_binary(session, trivia[1])
         yield smart_question_binary(session, trivia[2])
     else:
+        # We do two more questions
         yield smart_question_binary(session, questions[3])
         yield smart_question_binary(session, questions[4])
 
         yield session.call("rie.dialogue.say", text="Let's change things up.")
-
+        # Then we do the trivia question regardless
+        yield smart_question_binary(session, trivia[0])
+        yield smart_question_binary(session, trivia[1])
+        yield smart_question_binary(session, trivia[2])
         yield smart_question_binary(session, trivia[3])
         yield smart_question_binary(session, trivia[4])
 
-    ### Shall we start with trivia questions?
+    # Ask what were the user's favourite exercise was
     second_key = ["like", "I", "question", "one", "last", "about"]
     text = "Which question was your favourite one? Mine was question uhm... I forgot. Ahahaha."
-    reply, cert, final = yield keyword(session, text, second_key)
+    _, _, final = yield keyword(session, text, second_key)
+
     if final:
         yield session.call("rie.dialogue.say", text="That's a good one! I love it.")
 
