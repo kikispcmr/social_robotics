c4b5dabedc2c260802a8bb46656cde74caa3363f
Mathias Randruut <mathias.randruut@gmail.com>
2024-05-23 15:00:17 +0200

    main fixes

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -63,6 +63,7 @@ def main(session, details):
         yield robot_actions.move_negative()
     elif detected_emotion in positive_emotions:
         yield robot_actions.move_positive()
+    else: robot_actions.move_neutral()
     session.leave()
 
 
