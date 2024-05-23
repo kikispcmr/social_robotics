1446d145da32beb343ed5228f6f478ce3283188c
Kikis <kikispcmr@gmail.com>
2024-05-23 11:35:35 +0200

    pos mov

---
+++ assigment_3/drive.py
@@ -0,0 +1,17 @@
+
+
+class DriveSystem():
+
+    def __init__(self):
+        self.drive_meter = 0.5
+        self.perception_meter = {
+            "emotion1": 0,  
+                "emotion2" : 0, 
+    }
+
+        self.intensity_effects = {
+            
+        }
+
+
+
--- assignment_3/motivation_system.py
+++
@@ -1,5 +0,0 @@
-
-
-class MotivationSystem():
-    def __init__(self):
-        pass 
