d8155420e097666d622f7dd7914c40059b8ffbec
Kikis <kikispcmr@gmail.com>
2024-05-23 11:25:32 +0200

    Parts of pos movement done

--- assigment_3/robot_actions.py
+++ assigment_3/robot_actions.py
@@ -33,6 +33,39 @@ skiing_frames = [
         },
     ]
 
+positive_movement = [
+    # starting position
+        {
+            "time": 400,
+            "data": {
+                "body.arms.right.upper.pitch": -2.5,
+                "body.arms.left.upper.pitch": -2.5,
+            },
+        },
+        {
+            "time": 1000,
+            "data": {
+                "body.arms.right.lower.roll": -1.0,
+                "body.arms.left.lower.roll": 1.0,
+            },
+        },
+        """{
+            "time": 2000,
+            "data": {
+                "body.arms.right.upper.pitch": 1.0,
+                "body.arms.left.upper.pitch": -1.0,
+            },
+        },
+        # return to starting position
+        {
+            "time": 2400,
+            "data": {
+                "body.arms.right.upper.pitch": -0.5,
+                "body.arms.left.upper.pitch": -0.5,
+            },
+        },"""
+   ]
+
 class RobotActions:
 
 
@@ -41,6 +74,7 @@ class RobotActions:
         self.session = session
         self.movements = {
             "skiing": skiing_frames,
+            "pos" : positive_movement,
         }
 
     def touched(self, frame):
@@ -52,6 +86,6 @@ class RobotActions:
             # yield call("rie.dialogue.say", text="Ouch! Please don't touch me!")
             print("touch")
 
-    # Perform a specific movement from the internal dictionary of pre-built movements (made by us)
+    # Perform a specific movement from the internal dictionary of pre-built movements 
     def motion(self, movement: str):
         yield self.session.call("rom.actuator.motor.write", frames=self.movements[movement], force=True)
